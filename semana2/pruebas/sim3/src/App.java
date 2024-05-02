652b80b27f4235dba51cde308d2e2eada8b65c1a
Sebastian Gonzalez <sebasg97@outlook.com>
2024-05-02 15:53:57 -0600

    avance segunda semana

---
+++ semana2/pruebas/sim3/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String args[]) {
+        int i = 7;
+        if (i == 5)
+       //     System.out.println("Primer if");
+            if (i==7)
+                System.out.println("Segundo if");
+            else
+                System.out.println("Else");
+    }
+}

5372c4b8a8e088b5754dbd9a2e3e7eb0752a9728
Sebastian Gonzalez <sebasg97@outlook.com>
2024-05-18 19:29:36 -0600

    prueba IA

---
+++ semana4/codigos examen/test.java
@@ -0,0 +1,8 @@
+// genera un codigo que imprima hola mundo
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("adios mundo");
+    }
+}
